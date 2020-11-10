9de28a58adc224d6433493ac027c43cf859cf25b
Mykone Saunders <mykonesaunders@gmail.com>
2020-11-10 07:44:27 -0800

    LiveSync hash to prevent getCssFileName() error

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'nativescript-vue'
 import App from './components/App'
 import VueDevtools from 'nativescript-vue-devtools'
 
-if(TNS_ENV !== 'production') {
+if(TNS_ENV !== 'production' || false) {
   Vue.use(VueDevtools)
 }
 import store from './store'
@@ -15,3 +15,18 @@ new Vue({
   store,
   render: h => h('frame', [h(App)])
 }).$start()
+
+global.__onLiveSyncCore = function () {
+  var frame = require('@nativescript/core').Frame.topmost()
+  if (frame) {
+    if (frame.currentPage && frame.currentPage.modal) {
+      frame.currentPage.modal.closeModal()
+    }
+
+    if (frame.currentPage) {
+      frame.currentPage.addCssFile(
+          // require('@nativescript/core').getCssFileName() // Causing crash for HMR changes with the CopyWebpackPlugin bug
+      )
+    }
+  }
+}
